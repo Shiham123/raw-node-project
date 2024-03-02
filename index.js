36396e1c76103b050d01c1679ec949b093d93465
Shiham123 <s36651786@gmail.com>
2024-03-02 11:20:04 +0600

    CURD application complete based on raw node code

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const lib = require('./lib/data')
 
 const app = {}
 
-// ? sending file system to folder database
-// lib.create('test', 'newFile', { name: 'bangladesh', language: 'bangla' }, function (err) {
-// 	console.log('sending file from ')
+// sending file system to folder database
+// lib.create('test', 'newFile', { name: 'bangladesh', language: 'bangla' }, function (err, result) {
+// 	console.log('sending file from ', err, result)
 // })
 
-// ? readfile from folder database
-lib.read('test', 'newFile', function (err, result) {
-	console.log(`sending file from index file, error sending ${err} || result ${result}`)
+// readfile from folder database
+// lib.read('test', 'newFile', function (err, result) {
+// 	console.log(`sending file from index file, error sending ${err} || result ${result}`)
+// })
+
+// lib.update('test', 'newFile', { name: 'person one', email: 'personOne@email.com' }, (err, result) => {
+// 	console.log('sending from index.js ', err, result)
+// })
+
+lib.delete('test', 'newFile', (err, message) => {
+	console.log('delete file from index', err, message)
 })
 
 app.createServer = () => {
