00cc35f0f1c5d8b1493925fb60414118835b98cf
Shiham123 <s36651786@gmail.com>
2024-03-03 10:35:30 +0600

    token handler created

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const { sampleHandler } = require('../handler/routesHandler')
+const { tokenHandler } = require('../handler/tokenHandler')
 const { userHandler } = require('../handler/userHandler')
 
-const routes = { sample: sampleHandler, user: userHandler }
+const routes = { sample: sampleHandler, user: userHandler, token: tokenHandler }
 
 module.exports = routes
