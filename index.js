157b5c717744d7de460c96fb3436e86c8cf312c6
Shiham123 <s36651786@gmail.com>
2024-03-10 19:56:13 +0600

    part six complete

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const { handleReqRes } = require('./helpers/handleReqRes')
 const environmentExport = require('./helpers/environmentHandler')
 const { sendTwilioSms } = require('./helpers/notifications')
 
+/* eslint-disable no-console */
+
 const app = {}
 
 sendTwilioSms('01998992331', 'hello twilio', (err) => {
-	console.log('this is error', err.name)
+	console.log('this is error', err)
 })
 
 app.createServer = () => {
 	const server = http.createServer(app.handleResponse)
 	server.listen(environmentExport.port, () => {
-		/* eslint-disable no-console */
 		console.log(`listening port is ${environmentExport.port}`)
 	})
 }
