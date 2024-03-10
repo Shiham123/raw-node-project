6b3aa5a1db1ea6bb8ce75c9bea1645fc052c27d8
Shiham123 <s36651786@gmail.com>
2024-03-10 21:37:41 +0600

    some refactoring in the code server file and worker file created

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const http = require('http')
-const { handleReqRes } = require('./helpers/handleReqRes')
-const environmentExport = require('./helpers/environmentHandler')
-const { sendTwilioSms } = require('./helpers/notifications')
-
-/* eslint-disable no-console */
+const serverObject = require('./lib/server')
+const workerObject = require('./lib/worker')
 
 const app = {}
 
-sendTwilioSms('01998992331', 'hello twilio', (err) => {
-	console.log('this is error', err)
-})
-
-app.createServer = () => {
-	const server = http.createServer(app.handleResponse)
-	server.listen(environmentExport.port, () => {
-		console.log(`listening port is ${environmentExport.port}`)
-	})
+app.inti = () => {
+	serverObject.init()
+	workerObject.init()
 }
 
-app.handleResponse = handleReqRes
+app.inti()
 
-app.createServer()
+module.exports = app
